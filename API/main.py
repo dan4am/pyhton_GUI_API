09f05fef1cd93f31a65926fda08f21c2eeb62dda
Dan Freeman <dan4am@gmail.com>
2021-10-05 21:49:09 +0200

    Minor refactoring

--- API/main.py
+++ API/main.py
@@ -1,28 +1,74 @@
-from flask import Flask
-from flask_restful import Api, Resource,reqparse,abort
+import datetime
+
+import wrapper
+from flask import Flask, jsonify
+from flask_restful import reqparse
 
 app = Flask(__name__)
-api = Api(app)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:8452691@localhost/entreprise'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///entreprise-sqlite.db'
-
-video_put_args = reqparse.RequestParser()
-video_put_args.add_argument("name", type = str, help ="Name of the video is required", required =True)
-video_put_args.add_argument("views", type = str, help ="views of the video is required", required =True)
-video_put_args.add_argument("likes", type = str, help ="Likes of the video is required", required =True)
-
-videos ={}
 
 
 
-class Video(Resource):
-    def get(self, video_id):
-        return videos[video_id]
-    def put(self, video_id):
-        args  = video_put_args.parse_args()
-        return {video_id : args}
+@app.route('/apidan', methods=['GET'])
+def get_all_users():
+    result = wrapper.get_all_users()
+    if result:
+        return jsonify(status="True",
+        result= [{"nom": user.last_name,
+                    "prenom": user.first_name,
+                    "id": user.id,
+                    "birthdate": user.birthdate} for user in result.all() ])
+    return jsonify(status="False")
 
-api.add_resource(Video, "/video/<int:video_id>")
 
-if __name__ == "__main__":
-    app.run(debug = True)
+@app.route('/new', methods=['POST'])
+def new_user():
+    parser = reqparse.RequestParser()
+    parser.add_argument('id', type = int, help = 'id must be an int')
+    parser.add_argument('first_name' , help = 'id must be an string')
+    parser.add_argument('last_name', help = 'id must be an string')
+    parser.add_argument('birthdate', help = 'id must be a valid date')
+    args = parser.parse_args()
+    print(args)
+    result = wrapper.add_user(args)
+    if result:
+        return jsonify(status="True",
+        result= [{"nom": args['last_name'],
+                    "prenom": args['first_name'],
+                    "id": args['id'],
+                    "birthdate": args['birthdate']}])
+    return jsonify(status="False")
+
+@app.route('/apidan/user/<id>', methods = ['DELETE'])
+def delete_user(id):
+    temp = wrapper.get_user_by_id(id)
+    result = wrapper.delete_user(id)
+    if result:
+        return jsonify(status="True",
+                       result={"nom": temp.last_name,
+                               "prenom": temp.first_name,
+                               "id": temp.id,
+                               "birthdate": temp.birthdate}
+                       )
+    return jsonify(status="False")
+
+
+@app.route('/apidan/user/<id>', methods=['GET'])
+def get_user(id):
+
+    result = wrapper.get_user_by_id(id)
+    # print ("original")
+    if result:
+        return jsonify(status="True",
+                    result={"nom":result.last_name,
+                            "prenom":result.first_name,
+                            "id":result.id,
+                            "birthdate": result.birthdate}
+                        )
+    return jsonify(status="False")
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
